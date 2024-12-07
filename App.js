36956d008d34b4f6ba5213984cd359bfca808eea
chayi548 <167800726+Chay548@users.noreply.github.com>
2024-12-07 23:33:41 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const port=3000;
 app.get('/',(req,res)=>{
-    res.send("hi hello mg ra hk");
+    res.send("hi hello mg  hk");
 });
 app.listen(3000,"0.0.0.0",()=>{  
     console.log('listening on 3000');
